0e3910778a080b653149fb7cd2f841b7f3dc8a84
izacueio <izabella.coelhos@hotmail.com>
2022-08-23 19:29:59 -0400

    Exercicio 1
    Exercicio 1 sobre structs de compromissos, lista avaliativa

---
+++ Lista_Avaliativa_1/Ex1.cpp
@@ -0,0 +1,78 @@
+/* Faça um programa para simular uma agenda de compromissos e:
+ • Crie e leia um vetor de 5 estruturas de dados com: compromisso (máximo 60 letras) e data, nesta ordem. A data deve ser outra estrutura de dados contendo dia, mês e ano. 
+• Leia dois inteiros M e A e mostre todos os compromissos do mês M do ano A. Repita o procedimento até ler M = 0.
+Dica: utilize  scanf("%d/%d/%d", &variavel_dia, &variavel_mes, &variavel_ano); para leitura formatada da data.*/
+
+#include <stdio.h>
+#define DIM 100
+#define MAX 60
+
+struct tipoData{
+	int dia;
+	int mes;
+	int ano;	
+};
+
+struct tipoCompromissos{
+	char nome[MAX+1];
+	tipoData a;
+};
+
+
+// funcao para leitura do vetor
+void leitura(int n, tipoCompromissos compromissos[])
+{
+	for (int i = 0; i < 5; i++) 
+	{
+		printf(" Compromisso : ");
+		scanf(" %[^\n]", compromissos[i].nome);
+		printf(" Data do Compromisso: ");
+		scanf("%d/%d/%d", &compromissos[i].a.dia, &compromissos[i].a.mes,
+		&compromissos[i].a.ano);
+	}
+}
+
+// funcao para realizar a busca
+int consulta(tipoData data, int n, tipoCompromissos compromissos[])
+{
+	int i;
+	int achou = 0;	// nao foram encontrados compromissos
+	for (i = 0; i < n; i++){
+		if (compromissos[i].a.mes == data.mes && compromissos[i].a.ano == data.ano)
+		{
+			printf("%s\n", compromissos[i].nome);
+			achou = 1
+		}
+	}
+	return achou;
+}
+
+
+int main()
+{
+	tipoCompromissos compromissos[DIM];
+	int i, n;
+	tipoData data;
+
+	//printf("Informe o compromissos: ");
+	//scanf("%d", &n);
+	
+
+	// chamada da funcao leitura
+	leitura(n, compromissos);
+
+	printf("\nInforme uma data (mm/aaaa) -- (00/00 para encerrar): ");
+	scanf("%d/%d", &data.mes, &data.ano);
+	while( data.mes != 0 && data.ano != 0){
+		if(consulta(data, n, compromissos) == 0)
+			printf("Nenhum compromisso achado.\n");
+		
+		printf("\nInforme uma data (dd/mm) -- (00/00 para encerrar): ");
+		scanf("%d/%d", &data.dia, &data.mes);
+	}
+	
+	return 0;
+}
+
+
+
---
+++ Lista_Avaliativa_1/agendaExemplo.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#define DIM 100
+#define MAX 50
+/* Recebe um inteiro positivo n e n nomes, telefones e datas de
+aniversário, recebe uma data de consulta e mostra os nomes
+e telefones das pessoas que aniversariam nesta data */
+
+struct tipoData{
+	int dia;
+	int mes;
+	int ano;	
+};
+
+struct tipoAgenda{
+	char nome[MAX+1];
+	int telefone;
+	tipoData aniver;
+};
+
+
+// funcao para leitura do vetor
+void leitura(int n, tipoAgenda agenda[])
+{
+	for (int i = 0; i < n; i++) 
+	{
+		printf("\nAmigo(a): %3d\n", i+1);
+		printf(" Nome : ");
+		scanf(" %[^\n]", agenda[i].nome);
+		printf(" Telefone : ");
+		scanf("%d", &agenda[i].telefone);
+		printf(" Aniversário: ");
+		scanf("%d/%d/%d", &agenda[i].aniver.dia, &agenda[i].aniver.mes,
+		&agenda[i].aniver.ano);
+	}
+}
+
+// funcao para realizar a busca
+int consulta(tipoData data, int n, tipoAgenda agenda[])
+{
+	int i;
+	int achou = 0;	// nao foram encontrados aniversariantes
+	for (i = 0; i < n; i++){
+		if (agenda[i].aniver.dia == data.dia && agenda[i].aniver.mes == data.mes)
+		{
+			printf("%-50s %8d\n", agenda[i].nome, agenda[i].telefone);
+			achou = 1;
+		}
+	}
+	return achou;
+}
+
+
+int main()
+{
+	tipoAgenda agenda[DIM];
+	int i, n;
+	tipoData data;
+
+	printf("Informe a quantidade de amigos: ");
+	scanf("%d", &n);
+	
+	// chamada da funcao leitura
+	leitura(n, agenda);
+
+	printf("\nInforme uma data (dd/mm) -- (00/00 para encerrar): ");
+	scanf("%d/%d", &data.dia, &data.mes);
+	while( data.dia != 0 && data.mes != 0){
+		if(consulta(data, n, agenda) == 0)
+			printf("Nenhum aniversariante foi encontrado.\n");
+		
+		printf("\nInforme uma data (dd/mm) -- (00/00 para encerrar): ");
+		scanf("%d/%d", &data.dia, &data.mes);
+	}
+	
+	return 0;
+}
+
+
+
