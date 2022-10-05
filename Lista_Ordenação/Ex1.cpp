2f7c86612c53da2d60fb6ee1ee74134939a1603d
izacueio <izabella.coelhos@hotmail.com>
2022-10-05 13:11:13 -0400

    corrigindo a função imprime Reprovado
    corrigindo a função imprime Reprovado

--- Lista_Ordenação/Ex1.cpp
+++ Lista_Ordenação/Ex1.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define MAX 20
 #define DIM 100
+#include <string.h>
 
 struct alunos{
     char nome[MAX+1];
@@ -11,10 +12,8 @@ void leituraNomesPontuacao(int n, alunos a[])
 {
     for(int i = 0; i < n ; i++)
     {
-        printf("Insira o nome: ", i+1);
-        scanf(" %[^\n]", a[i].nome);
-        printf("Insira a quantidade de problemas resolvidos: \n");
-        scanf("%d", &a[i].problemas);
+        printf("Insira o nome e a quantidade de problemas resolvidos: ");
+        scanf("%s %d", a[i].nome, &a[i].problemas);
 
     }
 
@@ -23,21 +22,25 @@ void leituraNomesPontuacao(int n, alunos a[])
 void imprimeReprovado(int n, alunos a[]){
 
     int i, j, aux;
+    char auxstr[MAX];
 
     for(i = 0; i < n; i++){
        for(j = i+1; j < n; j++){
-            if(a[i].problemas >  a[j].problemas){
+            if(a[i].problemas <  a[j].problemas){
                 aux = a[i].problemas;
                 a[i].problemas = a[j].problemas;
                 a[j].problemas = aux;
-
             }
        }
     }
     for(int i = 0; i < n; i++){
-        printf("%s", a[i].nome);
+        printf("%d %s", a[i].problemas, a[i].nome);
+        
     }
 }
+
+
+
 
 
 
