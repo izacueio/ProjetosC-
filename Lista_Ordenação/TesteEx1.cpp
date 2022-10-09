4a72c4ce470d2499f51fa5edfb8573b4859cc5a3
izacueio <izabella.coelhos@hotmail.com>
2022-10-09 18:01:27 -0400

    questao 1
    att lista ordenação.

--- Lista_Ordenação/TesteEx1.cpp
+++ Lista_Ordenação/TesteEx1.cpp
@@ -8,6 +8,7 @@ struct alunos{
     int problemas;
 };
 
+
 //alunos a[MAX];
 
 void leituraNomesPontuacao(int n, alunos a[])
@@ -23,39 +24,32 @@ void leituraNomesPontuacao(int n, alunos a[])
 
 void imprimeReprovado(int n, alunos a[]){
 
-    int i, j, x, l, k = 0;
+    int i, j, x;
     char y[MAX];
 
 
 
-    if(a[k].problemas =! a[k+1].problemas){
     for(i = 1; i < n; i++){
         x = a[i].problemas;
         strcpy(y, a[i].nome);
-        for(j = i - 1; j >= 0 && a[j].problemas < x; j--){
+        for(j = i -1 ; j >= 0 && a[j].problemas < x; j--){
             a[j+1].problemas = a[j].problemas;
             strcpy(a[j+1].nome, a[j].nome);
                 }
         a[j+1].problemas = x;       
         strcpy(a[j+1].nome, y); 
-        }
-    }else {
-        for(k = 1; k < n; k++){
-            strcpy(y, a[k].nome);
-            for(l = k - 1; l >= 0 && strcmp(a[l].nome, a[k].nome) > 0; j--){
-                strcpy(a[k].nome, a[l].nome);
-            }
-            strcpy(a[l+1].nome, y);        
-            }
-            
-            }
-        }
+
+    }
+
+  }
+
 
 
 int main()
 {
 
 alunos a[DIM];
+
 
 int i;
 int n;
@@ -65,10 +59,11 @@ scanf("%d", &n);
 leituraNomesPontuacao(n, a);
 imprimeReprovado(n, a);
 
+        for(i= 0; i < n; i++){
+        printf("%s ", a[i].nome);
+   }
 
-for(i = 0; i < n; i++){
-    printf("%d %s ", a[i].problemas, a[i].nome);
-}
+
 
 return 0;
 
