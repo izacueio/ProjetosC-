903cc90075ebb6dcb06c715018c303c40b355a3f
izacueio <izabella.coelhos@hotmail.com>
2022-10-27 08:41:47 -0400

    Instalando o SpringBoot
    Instalando o SpringBoot

---
+++ TADS/tads-vscode/src/test/java/com/izacueio/tadsvscode/TadsVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izacueio.tadsvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TadsVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
